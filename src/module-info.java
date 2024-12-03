63b7ed34283dc8b645102af52b7595d6925c8e29
youssab-osama <Youssab235@gmail.com>
2024-12-03 20:16:48 +0200

    Initialize files

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+module cis.asu.watch_it {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens cis.asu.watch_it to javafx.fxml;
+    exports cis.asu.watch_it;
+}
