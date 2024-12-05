6e930e4dcf08ae377c1eabe31fda343b44b59fcd
NooR-El-Deen-Ramadan <165521766+NooR-El-Deen-Ramadan@users.noreply.github.com>
2024-12-05 22:36:04 +0200

    Add files via upload

---
+++ src/Actor.java
@@ -0,0 +1,18 @@
+package Crew_Members;
+
+import java.util.Date;
+import java.util.List;
+
+public class Actor extends Crew{
+
+    //constructor
+
+    public Actor(String firstName, String lastName, String gender, Date dateOfBirth, String nationality, int crewID) {
+        super(firstName, lastName, gender, dateOfBirth, nationality, crewID);
+    }
+
+
+
+
+
+}
---
+++ src/Director.java
@@ -0,0 +1,23 @@
+package Crew_Members;
+
+import java.util.Date;
+import java.util.List;
+
+public class Director extends Crew {
+    private List<String> Genres;
+    //constructor
+
+    public Director(String firstName, String lastName, String gender, Date dateOfBirth, String nationality, int crewID, List<String> genres) {
+        super(firstName, lastName, gender, dateOfBirth, nationality, crewID);
+        Genres = genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        Genres = genres;
+    }
+
+    public List<String> getGenres() {
+        return Genres;
+    }
+}
+
