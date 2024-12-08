c86ea034e5625dcc3e8991fe2191b06d4ea99732
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2024-12-08 22:22:32 +0200

    video merged.

--- src/main/java/mainPack/Main.java
+++ src/main/java/mainPack/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
+import javax.sound.sampled.Line;
 import java.io.File;
 import java.io.IOException;
 
